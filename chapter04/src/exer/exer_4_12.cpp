d2c7cbe7868f6d16884bbb7c0371658f246385f1
WJY_Hope@163.com <kakalot@DESKTOP-D4ATDUT.localdomain>
2026-03-26 22:14:12 +0800

    4_12

---
+++ chapter04/src/exer/exer_4_12.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file     exer_4_10.cpp
+ * @author   KaKaRot
+ * @date     25/3/2026
+ * @brief    PPP Chapter 4 exercise 10.
+ * @details  Judge if a number is prime
+ */
+
+ #include <iostream>
+ #include <vector>
+
+ int main()
+ {
+    const std::vector<int> prime_vec{2, 5, 7, 11, 12, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+    std::cout << "===========================================\n";
+    std::cout << "                 Prime Game                \n";
+    std::cout << "   Enter a number(0 ~100) ('q' to quit )   \n";
+    std::cout << "===========================================\n";
+
+
+    return 0;
+ }
