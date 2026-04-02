5ed26e927fdc99fe19dc9b8ac75bd21fd7e8ed7a
YLoveH-0531 <WJY_Hope@163.com>
2026-04-02 16:46:59 +0800

    update to 5_11

---
+++ chapter05/src/exer/exer_5_12_13.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file     exer_5_12_13.cpp
+ * @author   KaKaRot
+ * @date     2/4/2026
+ * @brief    PPP Chapter 5 exer 12, 13.
+ * @details  Bulls and Cows.
+ */
+
+ int main()
+ {
+    return 0;
+ }
