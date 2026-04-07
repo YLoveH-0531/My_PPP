85fcc270d6cf7e54f39968044fa5a441d3aae9e0
YLoveH-0531 <WJY_Hope@163.com>
2026-04-07 16:56:13 +0800

    jump to 7 drill

--- include/std_lib_facilities.h
+++ include/std_lib_facilities.h
@@ -85,7 +85,7 @@ template< class T> struct Vector : public std::vector<T>
 
     T& operator[]( unsigned int i ) // rather than return at(i);
     {
-        if( i < 0 || this->size() <= i ) throw Range_error( i );
+        if(this->size() <= i ) throw Range_error( i );
         return std::vector<T>::operator[]( i );
     }
     const T& operator[]( unsigned int i ) const
