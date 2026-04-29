70cbd7665cb685d42d6fe901cea9c9a83cb64999
YLoveH-0531 <WJY_Hope@163.com>
2026-04-29 17:05:30 +0800

    chapter 10 start

---
+++ chapter10/src/drill/Point.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file     Point.cpp
+ * @author   KaKaRot
+ * @date     29/4/2026
+ * @brief    Point class to exercise input and output
+ * @details  Point class to exercise input and output
+ */
+
+
+ #include <vector>
+ #include <iostream>
+
+struct Point {
+    double x;
+    double y;
+};
+
+std::vector<Point> original_points;
+
