bddd2ebb76ab6e095caf4065d4e2ddd3733416b0
YLoveH-0531 <WJY_Hope@163.com>
2026-05-06 17:13:43 +0800

    ch10 update

---
+++ chapter10/src/drill/Point_main.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file     Point_main.cpp
+ * @author   KaKaRot
+ * @date     29/4/2026
+ * @brief    Point class to exercise input and output
+ * @details  Point class to exercise input and output
+ */
+
+#include "Pointer.h"
+
+int main() {
+    read_points_with_cmd();
+    write_points_with_cmd();
+    return 0;
+}
