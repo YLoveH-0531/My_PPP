478b9b98cc2159b593446d802f114d07e51a5524
YLoveH-0531 <WJY_Hope@163.com>
2026-05-08 16:44:39 +0800

    ch10 update

---
+++ chapter10/src/exer/exer_10_6_7.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file     exer_10_6_7.cpp
+ * @author   KaKaRot
+ * @date     8/5/2026
+ * @brief    Roman_int class for holding Roman numerals 
+ * @details  Define a Roman_int class for holding Roman numerals
+ */
+
+
+ 
--- chapter10/src/exer/store_temps.cpp
+++ chapter10/src/exer/store_temps.cpp
@@ -3,8 +3,15 @@
  * @author   KaKaRot
  * @date     7/5/2026
  * @brief    creates a file of data in Reading type and fill the file 
- * @details  file name: raw_temps.txt.(exercise 10-3 10-4)
+ * @details  file name: raw_temps.txt.(exercise 10-2 10-4)
  */
 
 #include "Temp.h"
 
+void fill_from_vector(const std::vector<Readings>& record, std::ofstream& outfile, char terminator)
+{
+    for(const auto& r : record){
+        outfile << r.hour << ' ' << r.temperature << 'C' << '\n';
+    }
+    outfile << terminator << '\n';
+}
