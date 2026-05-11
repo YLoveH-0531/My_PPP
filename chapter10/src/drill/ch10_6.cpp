c3eea65d7b1e6ff516b2aa61b03b2115276d6f19
YLoveH-0531 <WJY_Hope@163.com>
2026-05-11 17:02:01 +0800

    chapter 10 u 2

--- chapter10/src/drill/ch10_6.cpp
+++ chapter10/src/drill/ch10_6.cpp
@@ -6,4 +6,19 @@
  * @details  rewrite some examples from chapter.
  */
 
- 
+#ifndef DATA_DIR
+#error "DATA_DIR must be defined by CMake. Use cmake to build this project."
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stdexcept>
+#include <vector>
+
+const std::string DATA = DATA_DIR;
+
+void fill_from_vector(const std::vector<int>& v, const std::string& str, char teminator)
+{
+    for(const auto&)
+}
