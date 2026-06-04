2d5c989a104122650c6324967fd8bfa738ce7428
YLoveH-0531 <WJY_Hope@163.com>
2026-06-04 21:50:58 +0800

    update skip_list

--- chapter18/include/exer/Skip_List.h
+++ chapter18/include/exer/Skip_List.h
@@ -12,10 +12,14 @@ class Skip_list
     private:
         node* head;
         int hight;
+        void destory();                                 // free all the node.
+        node* find(node* p);                            // find the node.
     public:
         Skip_list() : head(new node{0, 0 ,nullptr}), hight(0){}
-        Skip_list(const Skip_list& lhs);                // sholow copy
-        Skip_list& operator=(const Skip_list& lhs);     // sholow copy
+        Skip_list(const Skip_list& lhs);                // Shallow copy
+        Skip_list& operator=(const Skip_list& lhs);     // Shallow copy
+        Skip_list(Skip_list&& lhs) noexcept;
+        Skip_list& operator=(Skip_list&& lhs) noexcept;
         ~Skip_list();
 };
 
--- chapter18/src/exer/Skip_List.cpp
+++ chapter18/src/exer/Skip_List.cpp
@@ -10,22 +10,49 @@
  
 #include "Skip_List.h"
 
-Skip_list::Skip_list(const Skip_list& lhs)
+Skip_list::Skip_list(const Skip_list& lhs) : head(lhs.head), hight(lhs.hight)
 {
-
 }
 
 Skip_list& Skip_list::operator=(const Skip_list& lhs)
 {
+    if (this == &lhs) {
+        return *this;
+    }
+    destory();
+    head = lhs.head;
+    hight = lhs.hight;
+    return *this;
+}
 
+Skip_list::Skip_list(Skip_list&& lhs) noexcept : head(lhs.head), hight(lhs.hight)
+{
+    lhs.head = nullptr;
+    lhs.hight = 0;
+}
+
+Skip_list& Skip_list::operator=(Skip_list&& lhs) noexcept
+{
+    if (this == &lhs) {
+        return *this;
+    }
+    destory();
+    head = lhs.head;
+    hight = lhs.hight;
+    lhs.head = nullptr;
+    lhs.hight = 0;
+    return *this;
 }
 
 Skip_list::~Skip_list() 
 {
     hight = 0;
     node* p = head->succ[hight];
+    node* q = p;
     while (!p) {
-        node* p = 
+        q = p;
+        p = p->succ[hight];
+        delete q;
     }
     delete head; 
 }
