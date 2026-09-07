8b715ca15dd3afd012e70d85ede9f65f81439a36
YLoveH-0531 <WJY_Hope@163.com>
2026-09-07 22:38:29 +0800

    ch21 exer 9

--- chapter21/include/exer/Order.h
+++ chapter21/include/exer/Order.h
@@ -1,6 +1,7 @@
 #ifndef ORDER_H
 #define ORDER_H
 
+#include <ostream>
 #include <vector>
 #include <string>
 #include "date.h"
@@ -12,6 +13,34 @@ class Order{
     std::string Address;
     date::year_month_day order_date;
     std::vector<Purchase> BuyList;
+    
+    friend std::ostream& operator<<(std::ostream &os, const Order &order);
 };
 
+class Purchase{
+    private:
+    std::string Product;
+    int count;
+    double unit_price;
+    
+    friend std::ostream& operator<<(std::ostream &os, const Purchase &purchase);
+};
+
+std::ostream& operator<<(std::ostream &os, const Order &order)
+{
+    os << "Customer: " << order.Customer << "\n"
+                << "Address: " << order.Address << "\n"
+                << "Order Date: " << order.order_date << "\n"
+                << "Buy List: \n";
+    for (const auto& item : order.BuyList) {
+        os << item << "\n";
+    }
+}
+
+std::ostream& operator<<(std::ostream &os, const Purchase &purchase)
+{
+    os << "Product: " << purchase.Product << "\n"
+                << "Count: " << purchase.count << "\n"
+                << "Unit Price: " << purchase.unit_price << "\n";
+}
 #endif // ORDER_H
